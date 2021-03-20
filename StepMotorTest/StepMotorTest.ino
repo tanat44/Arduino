a472429030e0eab9ed2960acbb805da37b38b31f
tanat44 <tanat44@gmail.com>
2021-03-20 19:35:50 +0700

    step motor two direction

--- StepMotorTest/StepMotorTest.ino
+++ StepMotorTest/StepMotorTest.ino
@@ -1,115 +1,78 @@
-int pin1 = 2;
-int pin2 = 3;
-int pin3 = 4;
-int pin4 = 5;
+int pin1 = 0;
+int pin2 = 1;
+int pin3 = 2;
+int pin4 = 3;
+
+enum Direction {
+  Forward,
+  Backward
+};
+
+static int currentStep = 0;
+
+void driveTwoPhase(Direction x) {
+  if (x == Forward) {
+    if (currentStep > 3) {
+      currentStep = 0;
+    }
+  } else if (x == Backward) {
+    if (currentStep < 0) {
+      currentStep = 3;
+    }
+  }
+  
+  if (currentStep == 0) {
+    digitalWrite(pin1, HIGH); 
+    digitalWrite(pin2, HIGH); 
+    digitalWrite(pin3, LOW); 
+    digitalWrite(pin4, LOW); 
+  } else if (currentStep == 1) {
+    digitalWrite(pin1, LOW); 
+    digitalWrite(pin2, HIGH); 
+    digitalWrite(pin3, HIGH); 
+    digitalWrite(pin4, LOW); 
+  } else if (currentStep == 2) {
+    digitalWrite(pin1, LOW); 
+    digitalWrite(pin2, LOW); 
+    digitalWrite(pin3, HIGH); 
+    digitalWrite(pin4, HIGH); 
+  } else if (currentStep == 3) {
+    digitalWrite(pin1, HIGH); 
+    digitalWrite(pin2, LOW); 
+    digitalWrite(pin3, LOW); 
+    digitalWrite(pin4, HIGH); 
+  } 
+
+  if (x == Forward) {
+    ++currentStep;
+  } else if (x == Backward) {
+    --currentStep;
+  }
+}
+
+void moveToEnd(Direction d) {
+  for (int i=0; i<1250; ++i) {
+    driveTwoPhase(d);
+    delay(2);
+  }
+}
 
 void setup() {
-
   pinMode(pin1, OUTPUT);
   pinMode(pin2, OUTPUT);
   pinMode(pin3, OUTPUT);
   pinMode(pin4, OUTPUT);
+  moveToEnd(Backward);
 }
 
-void driveOnePhase( int x) {
-  if (x == 0) {
-    digitalWrite(pin1, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, LOW); 
-  } else if (x == 1) {
-    digitalWrite(pin2, HIGH); 
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, LOW); 
-  } else if (x == 2) {
-    digitalWrite(pin3, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin4, LOW); 
-  } else if (x == 3) {
-    digitalWrite(pin4, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin1, LOW); 
-  }
-}
-
-void driveTwoPhase( int x) {
-  if (x == 0) {
-    digitalWrite(pin1, HIGH); 
-    digitalWrite(pin2, HIGH); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, LOW); 
-  } else if (x == 1) {
-    digitalWrite(pin2, HIGH); 
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin3, HIGH); 
-    digitalWrite(pin4, LOW); 
-  } else if (x == 2) {
-    digitalWrite(pin3, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin4, HIGH); 
-  } else if (x == 3) {
-    digitalWrite(pin4, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin1, HIGH); 
-  }
-}
-
-void driveHalfStep( int x) {
-  if (x == 0) {
-    digitalWrite(pin1, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, LOW); 
-  } else if (x == 1) {
-    digitalWrite(pin1, HIGH); 
-    digitalWrite(pin2, HIGH); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, LOW);
-  } else if (x == 2) {
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin2, HIGH); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, LOW);
-  } else if (x == 3) {
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin2, HIGH); 
-    digitalWrite(pin3, HIGH); 
-    digitalWrite(pin4, LOW);
-  } else if (x == 4) {
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, HIGH); 
-    digitalWrite(pin4, LOW);
-  } else if (x == 5) {
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, HIGH); 
-    digitalWrite(pin4, HIGH);
-  } else if (x == 6) {
-    digitalWrite(pin1, LOW); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, HIGH);
-  } else if (x == 7) {
-    digitalWrite(pin1, HIGH); 
-    digitalWrite(pin2, LOW); 
-    digitalWrite(pin3, LOW); 
-    digitalWrite(pin4, HIGH);
-  }
-}
-
-static int currentStep = 0;
+static bool run = true;
   
 void loop() {
-
-  driveTwoPhase(currentStep++);
-  if (currentStep == 8){
-    currentStep = 0;
+  if (run) {
+    moveToEnd(Forward);
+  } else {
+    moveToEnd(Backward);
   }
-  delay(3);                
+  run = !run;
+  delay(1000);
 }
